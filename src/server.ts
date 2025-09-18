89527496106540106e9f139b406bc95fa5933ac1
Ian Barbosa <ian.barbosa@gestaoalmaz.com.br>
2025-09-18 14:46:50 -0300

    adicionando mensagem de erro na criação do filme

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,46 @@
-import express from 'express';
-import console = require('node:console');
-import { PrismaClient } from '../src/generated/prisma';
+import express from "express";
+import console = require("node:console");
+import { PrismaClient } from "../src/generated/prisma";
 
-const port = 3000
-const app = express()
+const port = 3000;
+const app = express();
 const prisma = new PrismaClient();
 
-app.get('/movies', async (_, res) =>{
-    const movies = await prisma.movie.findMany({
-        orderBy: {
-            title: "asc",
-        },
-        include: {
-            genres: true,
-            languages: true,
-        },
+app.use(express.json());
+
+app.get("/movies", async (_, res) => {
+  const movies = await prisma.movie.findMany({
+    orderBy: {
+      title: "asc",
+    },
+    include: {
+      genres: true,
+      languages: true,
+    },
+  });
+  res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+  try {
+    await prisma.movie.create({
+      data: {
+        title,
+        genre_id,
+        language_id,
+        oscar_count,
+        release_date: new Date(release_date),
+      },
     });
-    res.json(movies)
-})
+  } catch (error) {
+    return res.status(500).send({message: 'Falha ao cadastrar um filme'})
+  }
+
+  res.status(201).send();
+});
 
 app.listen(port, () => {
-    console.log(`Servidor iniciado na porta http://localhost/${port}`)
-})
+  console.log(`Servidor iniciado na porta http://localhost/${port}`);
+});
