47c8cee2779c2f8499b613c68f7f9183bdeb9fc6
Ian Barbosa <ian.barbosa@gestaoalmaz.com.br>
2025-09-19 10:25:20 -0300

    adicionado o filtra de filem por genero

--- src/server.ts
+++ src/server.ts
@@ -89,27 +89,48 @@ app.put("/movies/:id", async (req, res) => {
   res.status(200).send();
 });
 
-app.delete('/movies/:id', async (req, res) => {
-   const id = Number(req.params.id);
+app.delete("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id);
 
+  try {
+    const movie = await prisma.movie.findUnique({
+      where: { id },
+    });
 
-   try{
-     const movie = await prisma.movie.findUnique({
-     where: { id }})
+    if (!movie) {
+      return res.status(404).send({ message: "Filme não encontrado" });
+    }
 
- 
+    await prisma.movie.delete({ where: { id } });
+  } catch (error) {
+    res.status(500).send({ message: "Falha ao remover o registro" });
+  }
 
-     if (!movie) {
-        return res.status(404).send({ message: 'Filme não encontrado' });
-     }
+  res.status(200).send();
+});
 
-       await prisma.movie.delete({ where: { id }});
-   
-     }catch(error) {
-       res.status(500).send({ message: 'Falha ao remover o registro' });
-     }
-   
- res.status(200).send();
+app.get("/movies/:genderName", async (req, res) => {
+  const genderName = req.params.genderName
+  try {
+    const moviesFilteredByGenderName = await prisma.movie.findMany({
+      include: {
+        genres: true,
+        languages: true,
+      },
+      where: {
+        genres: {
+          name: {
+            equals: genderName,
+            mode: "insensitive",
+          },
+        },
+      },
+    });
+    res.status(200).send(moviesFilteredByGenderName)
+
+  } catch (error) {
+    return res.status(500).send({ message: "Falha ao atualizar um filme" })
+  }
 });
 
 app.listen(port, () => {
